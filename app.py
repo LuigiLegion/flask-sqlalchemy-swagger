b2a0e869dfc74c8f11346c02d3a0c663bf9f9370
Tal Luigi <luigi.t.agr@gmail.com>
2020-07-28 18:11:39 -0400

    feat: created the delete single product route

--- app.py
+++ app.py
@@ -103,6 +103,17 @@ def update_product(product_id):
     return product_schema.jsonify(product)
 
 
+# Delete single product
+@app.route('/products/<product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+
+    db.session.delete(product)
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+
 # Run server
 if __name__ == '__main__':
     app.run(debug=True)
